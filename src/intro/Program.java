dfcbabe554c23d40fc27dee21af15fb93ee7afc7
ajefi <ajefimo2001@gmail.com>
2022-01-30 14:31:59 +0200

    ex1

--- src/intro/Program.java
+++ src/intro/Program.java
@@ -1,5 +1,7 @@
 package intro;
 
+import java.util.Objects;
+
 public class Program {
 
     public static void main(String[] args) {
@@ -7,19 +9,33 @@ public class Program {
         int decimal = asDecimal("11001101");
 
         System.out.println(decimal); // 205
+        System.out.println(asString(11001101));
+        System.out.println(pow(3, 2));
     }
 
     public static String asString(int input) {
-        return "";
+        return String.valueOf(input);
     }
 
     public static int asDecimal(String input) {
-        return 0;
+        int ret = 0;
+        int temp = 1;
+        input = new StringBuilder(input).reverse().toString();
+        for (String c : input.split("")) {
+            if (Objects.equals(c, "1")) {
+                ret += temp;
+            }
+            temp = temp * 2;
+        }
+        return ret;
     }
 
     private static int pow(int arg, int power) {
-        // Java has Math.pow() but this time write your own implementation.
-
-        return 0;
+        int ret = 1;
+        while (power != 0) {
+            ret = ret * arg;
+            power--;
+        }
+        return ret;
     }
 }
