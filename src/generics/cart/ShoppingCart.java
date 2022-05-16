826e660c519c7b2479ce3f460609770bcace30b9
ajefi <ajefimo2001@gmail.com>
2022-05-16 18:13:37 +0300

    ex08

--- src/generics/cart/ShoppingCart.java
+++ src/generics/cart/ShoppingCart.java
@@ -3,98 +3,81 @@ package generics.cart;
 import java.util.*;
 
 public class ShoppingCart<T extends CartItem> {
+    private final LinkedList<T> cart = new LinkedList<>();
+    private final List<Double> discounts = new ArrayList<>();
 
-    Map<String, List> map = new HashMap<>();
-    ArrayList disc = new ArrayList();
-
-    public void add(Object item) {
-        CartItem cart = (CartItem) item;
-        List temp = new ArrayList();
-
-        if (map.containsKey(cart.getId())) {
-            increaseQuantity(cart.getId());
-        } else {
-            temp.add(cart.getPrice());
-            temp.add(1.0);
-
-            map.put(cart.getId(), temp);
-        }
+    public void add(T item) {
+        cart.add(item);
     }
 
     public void removeById(String id) {
-        map.remove(id);
+        for (T item : cart) {
+            if (item.getId().equals(id)) {
+                cart.remove(item);
+                break;
+            }
+        }
     }
 
     public Double getTotal() {
-        double sum = 0.0;
-        for (List elem : map.values()) {
-            double temp = (double) elem.get(0) * (double) elem.get(1);
-            sum += temp;
+        double total = 0D;
+        for (T item : cart) {
+            total += item.getPrice();
         }
-        for (Object discount : disc) {
-            sum = sum * (100 - (double) discount) / 100;
+        for (Double discount : discounts) {
+            total *= (100 - discount) / 100;
         }
-        return sum;
+
+        return total;
     }
 
     public void increaseQuantity(String id) {
-        List temp = new ArrayList();
-
-        temp.add(map.get(id).get(0));
-        temp.add((double) map.get(id).get(1) + 1.0);
-
-        map.replace(id, temp);
+        for (T item : cart) {
+            if (item.getId().equals(id)) {
+                add(item);
+                break;
+            }
+        }
     }
 
     public void applyDiscountPercentage(Double discount) {
-        disc.add(discount);
+        discounts.add(discount);
     }
 
     public void removeLastDiscount() {
-        disc.remove(disc.size() - 1);
+        discounts.remove(discounts.size() - 1);
     }
 
-    public void addAll(Object items) {
-        List<CartItem> carts = (List<CartItem>) items;
-        for (CartItem cart : carts) {
-            add(cart);
+    public void addAll(Collection<T> items) {
+        for (T item : items) {
+            add(item);
         }
     }
 
     @Override
     public String toString() {
+        HashMap<String, Integer> map = new HashMap<>();
 
-        Object[] a = map.keySet().toArray();
-
-        Collection<List> b = map.values();
-
-        StringBuilder ret = new StringBuilder();
-
-        double price;
-        double quantity;
-        int i = 0;
-
-        for (List o : b) {
-            price = (double) o.get(0);
-            quantity = (double) o.get(1);
-
-            ret.append("(");
-            ret.append(a[i]);
-
-            ret.append(", ");
-            ret.append(price);
-
-            ret.append(", ");
-            ret.append((int) quantity);
-
-            if (i + 1 == map.keySet().size()) {
-                ret.append(")");
-            } else {
-                ret.append("), ");
-            }
-            i++;
+        for (T item : cart) {
+            map.compute(item.getId(), (k, v) -> (v == null) ? 1 : (v + 1));
         }
 
-        return ret.toString();
+        StringBuilder sb = new StringBuilder();
+        for (T item : cart) {
+
+            if (!map.containsKey(item.getId())) {
+                continue;
+            }
+            String s = "(" + item.getId() + ", " + item.getPrice() + ", " + map.get(item.getId()) + ")";
+
+            map.remove(item.getId());
+            if (!map.keySet().isEmpty()) {
+                s += ", ";
+            }
+
+            sb.append(s);
+        }
+
+        return sb.toString();
     }
 }
